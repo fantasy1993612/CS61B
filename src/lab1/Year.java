4e87594588360b169aeb3282b89cd60cc61ba3ad
fantasy1993612 <1265911780@qq.com>
2015-07-08 19:04:25 +0800

    lab1->Year.java

---
+++ src/lab1/Year.java
@@ -0,0 +1,43 @@
+package lab1;
+public class Year {
+
+    /** @param  year to be analyzed
+     *  @return true if year is a leap year
+     *          false if year is not a leap year
+     */
+    static boolean isLeapYear(int year) {
+    	if ((year%400==0) || ((year%4==0) && (year%100!=0))){
+    		return true;
+    	}
+        return false;    // YOUR CODE HERE
+    }
+
+    /** Calls isLeapYear to print correct statement.
+     *  @param  year to be analyzed
+     */
+    private static void checkLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.printf("%d is a leap year.\n", year);
+        } else {
+            System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    /** Must be provided an integer as a command line argument ARGS. */
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please enter command line arguments.");
+            System.out.println("e.g. java Year 2000");
+        }
+        for (int i = 0; i < args.length; i++) {
+            try {
+                int year = Integer.parseInt(args[i]);
+                checkLeapYear(year);
+            } catch (NumberFormatException e) {
+                System.out.printf("%s is not a valid number.\n", args[i]);
+            }
+        }
+    }
+
+}
+
